f1e75d48a1d0df528fe9c1d024d073b5bf876fad
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2021-05-15 14:56:52 +0530

    [+] Fixed bugs and added ping module

--- lib/deepnoodlelib.py
+++ lib/deepnoodlelib.py
@@ -1,7 +1,8 @@
-import random, string, requests
+import random, string, requests, base64
 from time import sleep
-BASE_PREFIX = 'https://deepnude.to'
+from requests.exceptions import ConnectTimeout
 
+BASE_PREFIX = str(base64.b64decode('aHR0cHM6Ly9kZWVwbnVkZS50bw==').decode('utf-8'))
 TOKLEN_SESSIONID = 32
 TOKLEN_IMGID = 15
 
@@ -16,18 +17,25 @@ def generate_random_token(toklen):
     token = ''.join(random.choices(string.ascii_lowercase + string.digits, k = toklen))
     return token
 
+def check_ping():
+    try:
+        r=requests.get(url=BASE_PREFIX+ url_prefixes.status, timeout=(25, 30)).status_code
+    except ConnectTimeout:
+        r = 408
+    return r
+    
 def get_upload_perm(image_id, cookie):
     headers = {
-    'Host': 'deepnude.to',
+    'Host': BASE_PREFIX.split('/')[2],
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0',
     'Accept': '*/*',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
     'X-Requested-With': 'XMLHttpRequest',
-    'Origin': 'https://deepnude.to',
+    'Origin': '{}'.format(BASE_PREFIX),
     'DNT': '1',
     'Connection': 'keep-alive',
-    'Referer': 'https://deepnude.to/',
+    'Referer': '{}'.format(BASE_PREFIX),
     'Cookie': 'userid={0}; identifier={0}'.format(cookie),
     'Pragma': 'no-cache',
     'Cache-Control': 'no-cache',
@@ -35,12 +43,12 @@ def get_upload_perm(image_id, cookie):
     }
     url = BASE_PREFIX + url_prefixes.upload_request + image_id
     r = requests.post(url=url, headers=headers)
-    print(r.content.decode('utf-8'))
+    return r.content.decode('utf-8')
 
 def upload_image(image_id, cookie, filename):
     data = open(filename, 'rb').read()
     headers = {
-    'Host': 'deepnude.to',
+    'Host': BASE_PREFIX.split('/')[2],
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0',
     'Accept': '*/*',
     'Accept-Language': 'en-US,en;q=0.5',
@@ -48,10 +56,10 @@ def upload_image(image_id, cookie, filename):
     'Content-Type': 'image/jpeg',
     'X-Requested-With': 'XMLHttpRequest',
     'Content-Length':'{0}'.format(str(len(data)-3)),
-    'Origin': 'https://deepnude.to',
+    'Origin': '{}'.format(BASE_PREFIX),
     'DNT': '1',
     'Connection': 'keep-alive',
-    'Referer': 'https://deepnude.to/',
+    'Referer': '{}'.format(BASE_PREFIX),
     'Cookie': 'userid={0}; identifier={0}'.format(cookie),
     'Pragma': 'no-cache',
     'Cache-Control': 'no-cache',
@@ -59,19 +67,19 @@ def upload_image(image_id, cookie, filename):
     }
     url = BASE_PREFIX + url_prefixes.image_upload + image_id
     r = requests.put(url=url, headers=headers, data=data)
-    print(r.status_code)
+    return r.status_code
 
 def get_image(image_id, cookie):
     sleep(20)
     headers={
-    'Host': 'deepnude.to',
+    'Host': BASE_PREFIX.split('/')[2],
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0',
     'Accept': 'image/webp,*/*',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
     'DNT': '1',
     'Connection': 'keep-alive',
-    'Referer': 'https://deepnude.to/',
+    'Referer': '{}'.format(BASE_PREFIX),
     'Cookie': 'userid={0}; identifier={0}'.format(cookie),
     'Pragma': 'no-cache',
     'Cache-Control': 'no-cache',
@@ -79,4 +87,4 @@ def get_image(image_id, cookie):
     }
     url = BASE_PREFIX + url_prefixes.get_im_prefix + image_id + url_prefixes.get_im_suffix
     rs = requests.get(url=url, headers=headers)
-    return rs.content
+    return rs.status_code, rs.content
